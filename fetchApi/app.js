287a10327a23f96ca01fa69888d0b557e1a2587d
Saud <gomail2017@gmail.com>
2021-08-17 23:52:10 +0500

    fetchApi from textFile, jsonFile and from external Api

--- fetchApi/app.js
+++ fetchApi/app.js
@@ -1,37 +1,38 @@
 document.getElementById('btn1').addEventListener('click', fromTxt)
 document.getElementById('btn2').addEventListener('click',fromJSON)
-//document.getElementById('btn3').addEventListener('click', fromExternal)
+document.getElementById('btn3').addEventListener('click', fromExternal)
 
-function fromTxt(){
+ const fromTxt=()=>{
     fetch('text.txt')
-    .then(function(res){
-        return (res.text())
+    .then((res)=>{
+        return res.text()
     })
-    .then(function(res){
-        console.log(res)
-    })
-    .catch(function(err){
-        console.log(err)
-    })
+    .then(res => console.log(res))
+    .catch(err => console.log(err))
 }
 
 function fromJSON(){
     fetch('text.json')
+    .then(res => res.json())
     .then(function(res){
         console.log(res)
+        res.forEach(val => console.log(val.name))
     })
-    .catch(function(err){
-        console.log(err)
-    })
+    .catch(err => console.log(err))
 }
-/*
+
 function fromExternal(){
-    fetch('text.txt')
+    fetch('https://api.github.com/users')
+    .then(function(res){
+        return (res.json())
+    })
     .then(function(res){
         console.log(res)
+        
+        res.forEach(function(val){
+            console.log(val.login)})
     })
     .catch(function(err){
         console.log(err)
     })
 }
-*/
